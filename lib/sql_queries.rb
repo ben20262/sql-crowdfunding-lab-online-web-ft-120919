2e179b85f6d1e37c4fcad5b012e52cb05845b6c9
Benjamin Small <ben20262@aim.com>
2020-01-23 19:18:23 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -49,5 +49,6 @@ def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_b
   "SELECT projects.category, SUM(pledges.amount)
   FROM pledges
   JOIN projects ON pledges.project_id = projects.id
+  WHERE projects.category = 'books'
   GROUP BY projects.category;"
 end
