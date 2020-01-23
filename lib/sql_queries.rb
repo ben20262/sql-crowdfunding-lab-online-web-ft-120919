0710cc3f3728c840b56ec9ac3bea10535c98e245
Benjamin Small <ben20262@aim.com>
2020-01-23 16:58:01 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
   FROM pledges
   JOIN users ON pledges.user_id = users.id
   GROUP BY users.name
-  ORDER BY users.name"
+  ORDER BY users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
@@ -27,7 +27,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
   FROM pledges
   JOIN projects ON pledges.project_id = projects.id
   GROUP BY project.id
-  HAVING sum(pledges.amount) > projects.funding_goal"
+  HAVING sum(pledges.amount) > projects.funding_goal;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
