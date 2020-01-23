038dcfc855d0f1c6b97acc8777feb8866c5213a8
Benjamin Small <ben20262@aim.com>
2020-01-23 16:15:52 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (name, age) VALUES ("Bob", 10), ("Lou", 11), ("Ben", 12), ("Tom", 13), ("Dylan", 14), ("Kathrine", 15), ("Joe", 16), ("Sunny", 17),
+("Al", 18), ("Becky", 13), ("Lexi", 16), ("Margo", 18), ("PeeWee", 13), ("Roxanne", 12), ("Tucker", 9), ("Charlie", 3), ("Boy", 20), ("Aaron", 43),
+("Lewis", 22), ("That Guy", 10);
