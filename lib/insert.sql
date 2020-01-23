ddbda627f8a4619e4faa4acce504f454e8ca3491
Benjamin Small <ben20262@aim.com>
2020-01-23 16:22:29 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,9 +2,9 @@ INSERT INTO users (name, age) VALUES ("Bob", 10), ("Lou", 11), ("Ben", 12), ("To
 ("Al", 18), ("Becky", 13), ("Lexi", 16), ("Margo", 18), ("PeeWee", 13), ("Roxanne", 12), ("Tucker", 9), ("Charlie", 3), ("Boy", 20), ("Aaron", 43),
 ("Lewis", 22), ("That Guy", 10);
 
-INSERT INTO projects VALUES ("Meme Wars", "entertainment", 1000, "1-23-2020", "2-23-2020"), ("Meme Wars", "entertainment", 1000, "1-23-2020", "2-23-2020"),
-("Make Star Wars Great Again", "entertainment", 10000000, "1-23-2020", "2-23-2020"), ("I want money", "charity", 100000000, "1-23-2020", "2-23-2020"),
-("Homeless", "charity", 5, "1-23-2020", "2-23-2020"), ("I Like Trains", "entertainment", 1000, "1-23-2020", "2-23-2020"),
-("Racecar", "entertainment", 2500, "1-23-2020", "2-23-2020"), ("How to make kickstarter?", "help", 1, "1-23-2020", "2-23-2020"),
-("This", "entertainment", 1000, "1-23-2020", "2-23-2020"), ("That", "entertainment", 1000, "1-23-2020", "2-23-2020"),
-("The other", "entertainment", 1000, "1-23-2020", "2-23-2020");
+INSERT INTO projects (names, category, funding_goal, start_date, end_date) VALUES ("Meme Wars", "entertainment", 1000, "1-23-2020", "2-23-2020"),
+("Meme Wars", "entertainment", 1000, "1-23-2020", "2-23-2020"), ("Make Star Wars Great Again", "entertainment", 10000000, "1-23-2020", "2-23-2020"),
+("I want money", "charity", 100000000, "1-23-2020", "2-23-2020"), ("Homeless", "charity", 5, "1-23-2020", "2-23-2020"),
+("I Like Trains", "entertainment", 1000, "1-23-2020", "2-23-2020"), ("Racecar", "entertainment", 2500, "1-23-2020", "2-23-2020"),
+("How to make kickstarter?", "help", 1, "1-23-2020", "2-23-2020"), ("This", "entertainment", 1000, "1-23-2020", "2-23-2020"),
+("That", "entertainment", 1000, "1-23-2020", "2-23-2020"), ("The other", "entertainment", 1000, "1-23-2020", "2-23-2020");
