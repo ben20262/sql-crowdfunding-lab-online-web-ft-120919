ddd22d21edb3ad8ef41600681ded04f91b7c853c
Benjamin Small <ben20262@aim.com>
2020-01-23 16:22:57 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,7 +2,7 @@ INSERT INTO users (name, age) VALUES ("Bob", 10), ("Lou", 11), ("Ben", 12), ("To
 ("Al", 18), ("Becky", 13), ("Lexi", 16), ("Margo", 18), ("PeeWee", 13), ("Roxanne", 12), ("Tucker", 9), ("Charlie", 3), ("Boy", 20), ("Aaron", 43),
 ("Lewis", 22), ("That Guy", 10);
 
-INSERT INTO projects (names, category, funding_goal, start_date, end_date) VALUES ("Meme Wars", "entertainment", 1000, "1-23-2020", "2-23-2020"),
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Meme Wars", "entertainment", 1000, "1-23-2020", "2-23-2020"),
 ("Meme Wars", "entertainment", 1000, "1-23-2020", "2-23-2020"), ("Make Star Wars Great Again", "entertainment", 10000000, "1-23-2020", "2-23-2020"),
 ("I want money", "charity", 100000000, "1-23-2020", "2-23-2020"), ("Homeless", "charity", 5, "1-23-2020", "2-23-2020"),
 ("I Like Trains", "entertainment", 1000, "1-23-2020", "2-23-2020"), ("Racecar", "entertainment", 2500, "1-23-2020", "2-23-2020"),
